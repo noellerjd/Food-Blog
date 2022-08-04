5b6d5a72a9a47d4da5475beca08d50e66d78d6b7
noellerjd <noellerninja@gmail.com>
2022-08-03 20:00:17 -0600

    SQL database setup

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+const Sequelize = require("sequelize");
+
+// Create a connection object
+const sequelize = new Sequelize(
+  // Database name
+  process.env.DB_NAME,
+  // User
+  process.env.DB_USER,
+  // Password
+  process.env.DB_PASSWORD,
+  {
+    // Database location
+    host: "localhost",
+    dialect: "mysql",
+    port: 3306,
+  }
+);
+
+module.exports = sequelize;
