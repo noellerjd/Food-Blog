4768e4216ffc674277c2f602b6b37791999fe66a
vanessaroman <vanessavirginiarr@gmail.com>
2022-08-08 19:14:28 -0600

    router issues

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
-const { Sequelize } = require("sequelize");
-const sequelize = require("sequelize");
+
+const Sequelize = require("sequelize");
 require("dotenv").config();
 
 const sequelize = new Sequelize(
