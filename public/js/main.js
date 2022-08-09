6c176a40c7162fdf8daaa832f8a3889f3a6df7d0
vanessaroman <vanessavirginiarr@gmail.com>
2022-08-08 19:00:43 -0600

    handlebars update

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,10 @@
-// const revButton = document.getElementsByClassName('review-button');
-// const revDiv = document.getElementsByClassName('review-recipe-form');
+const revButton = document.getElementsByClassName('review-button');
+const revDiv = document.getElementsByClassName('review-recipe-form');
 
-// revButton.onclick = function () {
-//     if (revDiv.style.display !== "none") {
-//       revDiv.style.display = "none";
-//     } else {
-//       revButton.style.display = "block";
-//     }
-//   };
+revButton.onclick = function () {
+    if (revDiv.style.display !== "none") {
+      revDiv.style.display = "none";
+    } else {
+      revButton.style.display = "block";
+    }
+  };
