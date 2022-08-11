5e7d3476bddce22366b551c05fa8b35cd058eb92
AugustusXI <103222292+AugustusXI@users.noreply.github.com>
2022-08-10 21:48:21 -0600

    Changed things around in app.js

--- controllers/api/app.js
+++ controllers/api/app.js
@@ -1,54 +1,51 @@
 const express = require("express");
 const app = express();
-// const post = require("../models/BlogPost");
+const Post = require("../../models/Review");
 const router = express.Router();
 
-const reviewInfo = req.body;
-
-function postReview() {
-  // return the json objects of the review
-  router.post("/", async (req, res) => {
-    const review = new Review({
-      id: req.body.id,
-      user: req.body.user,
-      body: req.body.body,
-      date: req.body.date,
-      likes: req.body.likes,
-    });
-    // pass in the json objects as parameters
-    //  add post to database
-    db.add(reviewInfo).then((review) => {
-      res
-        .status(201)
-        .json({ review })
-        .catch((err) => {
-          res.status(500).json({ err });
-        });
-    });
+// gets back all the posts
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find();
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+// return the json objects of the review
+router.post("/", async (req, res) => {
+  console.log("hello");
+  const reviewInfo = new Review({
+    id: req.body.id,
+    user: req.body.user,
+    body: req.body.body,
+    date: req.body.date,
+    likes: req.body.likes,
   });
-}
+  console.log(reviewInfo);
+  //  add review to database
+  db.add(reviewInfo).then((review) => {
+    res
+      .status(201)
+      .json({ review })
+      .catch((err) => {
+        res.status(500).json({ err });
+      });
+  });
+});
+
 // a way to add posts
 router.post("/", async (req, res) => {
   const post = new Post({
     id: req.body.id,
     user: req.body.user,
-    post_description: req.body.post_description,
-    post_date: req.body.post_date,
+    body: req.body.body,
+    date: req.body.date,
     likes: req.body.likes,
   });
 
   try {
     const savedPost = await post.save();
     res.json(savedPost);
-  } catch (err) {
-    res.json({ message: err });
-  }
-});
-
-// gets back all the posts
-router.get("/", async (req, res) => {
-  try {
-    const posts = await Post.find();
   } catch (err) {
     res.json({ message: err });
   }
