a88441f86b6d253eced31635ec83a9b4a1da1f34
noellerjd <noellerninja@gmail.com>
2022-08-12 18:05:57 -0600

    index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,7 @@
 // Establishes router to use the .router() portion from express
 const router = require("express").Router();
 // Establises the obj userRoutes to require the userRoutes.js
-const userRoutes = require("./userRoutes");
+const userRoutes = require("../userRoutes");
 
 // not completely sure yet
 router.use("/users", userRoutes);
